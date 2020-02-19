86dceebaf4d25eb7bf686a663edfe6ab2196a9bb
ChristophLarson <clarso5@lsu.edu>
2020-02-19 09:52:33 -0600

    Begin coding

--- sudokuChecker.cpp
+++ sudokuChecker.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <cstdlib>
+#include <pthread.h>
 using namespace std;
 
+const int numThreads = 11;
+
+
+//structure for passing data to threads
+struct parameters
+{
+	int row;
+	int column;
+};
+
 int main() {
-  cout << "Hello, World!";
+
+	parameters *data = (parameters*) malloc(sizeof(parameters));
+
+
   return 0;
 }
